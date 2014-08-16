0d2466379f95d2be156194eb1bc240c7a4964f7e
Mohit Shridhar <mohit.appi@gmail.com>
2014-08-16 17:12:26 +0800

    basic door controller

--- src/plugins/flip_door_plugin.cc
+++ src/plugins/flip_door_plugin.cc
@@ -15,12 +15,6 @@
 
 #define PI_HALF 1.57
 
-//ps3 controller service requests:
-#define REQUEST_UP_BTN "up_btn"
-#define REQUEST_DOWN_BTN "down_btn"
-#define REQUEST_RIGHT_BTN "right_btn"
-#define REQUEST_LEFT_BTN "left_btn"
-
 #define CONTEXT_SPACE_X_RANGE 2.0 // in m
 #define CONTEXT_SPACE_Y_RANGE 2.0
 #define CONTEXT_SPACE_Z_RANGE 2.0
@@ -29,37 +23,144 @@
 namespace gazebo
 { 
 
-  typedef const boost::shared_ptr<const msgs::Pose> PosePtr;
-
   class FlipDoor : public ModelPlugin
   {
 
-    private:
-      ros::NodeHandle *node;
+  private:
+    physics::ModelPtr model;
+    physics::LinkPtr doorLink;
+    math::Pose currPose, currFpvPose; 
 
-    public:
+    math::Vector3 cmd_vel;
 
-      FlipDoor()
-      {
-        std::string name = "ros_door_plugin_node";
-        int argc = 0;
-        ros::init(argc, NULL, name);
+    bool isActive;
+    int activeDoors[100];
+    
+    int door_ref_num;
+    std::string door_model_name, door_direction, model_domain_space;
 
+    ros::NodeHandle* rosNode;
+    transport::NodePtr gazeboNode;
+    event::ConnectionPtr updateConnection;
+
+    transport::SubscriberPtr subFpvPose, subGzRequest;
+    ros::Subscriber sub, sub_active;
+
+  public:
+    FlipDoor()
+    {
+      std::string name = "flip_door_plugin_node";
+      int argc = 0;
+      ros::init(argc, NULL, name);
+
+    }
+    ~FlipDoor()
+    {
+      delete rosNode;
+    }
+
+    void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+    {
+      establishLinks(_parent);
+      determineRotationType(_sdf);
+      determineModelDomain(_sdf);
+      initVars();
+    }
+
+    void OnUpdate()
+    {
+      ros::spinOnce();
+      doorLink->SetAngularVel(cmd_vel);
+    }
+
+  private:
+    void determineRotationType(sdf::ElementPtr _sdf)
+    {
+      if (!_sdf->HasElement("door_direction")) {
+        ROS_ERROR("Door direction not specified in the plugin reference");
+        std::exit(EXIT_FAILURE);
       }
-      ~FlipDoor()
-      {
-        delete this->node;
+
+      door_direction = _sdf->GetElement("door_direction")->Get<std::string>();
+
+      if (door_direction.compare("clockwise") != 0 && door_direction.compare("counter_clockwise") != 0) {
+        ROS_ERROR("Invalid door direction specified. Only two states possible: 'clockwise' & 'counter_clockwise'");
       }
 
-      void Load(physics::ModelPtr _parent, sdf::ElementPtr)
-      {
+    }
 
+    void determineModelDomain(sdf::ElementPtr _sdf)
+    {
+      if (!_sdf->HasElement("model_domain_space")) {
+        ROS_ERROR("Model Domain Space not specified in the plugin reference");
+        std::exit(EXIT_FAILURE);
       }
 
-      void OnUpdate()
-      {
+      model_domain_space = _sdf->GetElement("model_domain_space")->Get<std::string>();
 
+      ROS_INFO("Door '%s' initialized - Direction: %s, Domain Space: %s\n", door_model_name.c_str(), door_direction.c_str(), model_domain_space.c_str());
+    }
+
+    void initVars()
+    {
+      isActive = false;
+
+      std::string door_ref_num_str = door_model_name; 
+      replaceSubstring(door_ref_num_str, model_domain_space, "");
+      door_ref_num = atoi(door_ref_num_str.c_str());
+    }
+
+    void establishLinks(physics::ModelPtr _parent)
+    {
+      model = _parent;
+      doorLink = model->GetLink("door");
+      door_model_name = model->GetName();
+
+      rosNode = new ros::NodeHandle("/door_controller");
+
+      sub = rosNode->subscribe<geometry_msgs::Twist>("/door_controller/command", 1000, &FlipDoor::cmd_ang_cb, this );
+      sub_active = rosNode->subscribe<std_msgs::UInt32MultiArray>("/door_controller/active", 1000, &FlipDoor::active_doors_cb, this);
+
+      updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&FlipDoor::OnUpdate, this));
+    }
+
+    void cmd_ang_cb(const geometry_msgs::Twist::ConstPtr& msg)
+    {
+      if (isActive) {
+        setAngularVel(msg->angular.z);
+        ROS_INFO("Door '%s' - Angular z: [%f]", door_model_name.c_str(), msg->angular.z);
       }
+    }
+
+    void setAngularVel(float angularVel)
+    {
+      if (door_direction.compare("clockwise") == 0) { 
+        cmd_vel.z = angularVel;
+      } else {
+        cmd_vel.z = -angularVel; 
+      }
+    }
+
+    void active_doors_cb(const std_msgs::UInt32MultiArray::ConstPtr& array) 
+    {
+      isActive = false;
+      
+      int i=0;
+
+      for (std::vector<uint32_t>::const_iterator it = array->data.begin(); it != array->data.end(); ++it) {
+        activeDoors[i] = *it;
+        i++;
+
+        if (*it == door_ref_num) {
+          isActive = true;
+        }
+      }
+    }
+
+    std::string replaceSubstring(std::string &s, std::string toReplace, std::string replaceWith)
+    {
+      return(s.replace(s.find(toReplace), toReplace.length(), replaceWith));
+    }
 
   };
 
