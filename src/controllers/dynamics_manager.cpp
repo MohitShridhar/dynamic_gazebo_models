144b14dce1d9c40ef4e159051b1504390669bd8b
Mohit Shridhar <mohit.appi@gmail.com>
2014-08-17 17:34:07 +0800

    First working version of group manager

--- src/controllers/dynamics_manager.cpp
+++ src/controllers/dynamics_manager.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
+#include <algorithm>
 #include <ros/ros.h>
 
-#include "control_group.cpp"
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/UInt32MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/UInt8.h>
 
+#include "control_group.h"
+
+#include <dynamic_models/ControlGroup.h>
 #include <dynamic_models/AddGroup.h>
 #include <dynamic_models/DeleteGroup.h>
+#include <dynamic_models/ListGroups.h>
+#include <dynamic_models/OpenCloseDoors.h>
+#include <dynamic_models/OpenCloseElevDoors.h>
+#include <dynamic_models/SetElevProps.h>
+#include <dynamic_models/SetVelDoors.h>
+#include <dynamic_models/TargetFloorElev.h>
 
 #define TYPE_DOOR_STR "door"
 #define TYPE_ELEVATOR_STR "elevator"
+
+#define DEFAULT_SLIDE_SPEED 1 // in m/s
+#define DEFAULT_FLIP_SPEED 1.57 // in rad/s
+
+#define STATE_OPEN true
+#define STATE_CLOSE false
+
+#define INDEX_NOT_FOUND -1
+
 
 class DynamicsController
 {
 	private:
 
 		ros::NodeHandle rosNode;
-		ros::ServiceServer add_group_server, delete_group_server;
+		ros::ServiceServer add_group_server, delete_group_server, list_groups_server;
+		ros::ServiceServer open_close_doors_server, set_vel_doors_server, target_floor_elev_server, set_elev_props_server, open_close_elev_doors_server;
+		
+		ros::Publisher door_cmd_vel_pub, door_active_pub;
+		ros::Publisher elev_cmd_floor_pub, elev_active_pub, elev_param_pub, elev_door_pub;
 
-		std::vector<ControlGroup> groups;		
+		std::vector<ControlGroup> groups;
 
 	public:
 
@@ -25,31 +52,150 @@ class DynamicsController
 			rosNode = nh;
 			nh = ros::NodeHandle("");
 
+			setupControlTopics();
 			setupManagerServices();
-
 		}
 
 		void setupManagerServices()
 		{
 			add_group_server = rosNode.advertiseService("model_dynamics_manager/add_control_group", &DynamicsController::add_control_group_cb, this);
 			delete_group_server = rosNode.advertiseService("model_dynamics_manager/delete_control_group", &DynamicsController::delete_control_group_cb, this);
+			list_groups_server = rosNode.advertiseService("model_dynamics_manager/list_groups", &DynamicsController::list_groups_cb, this);
+
+			open_close_doors_server = rosNode.advertiseService("model_dynamics_manager/doors/open_close", &DynamicsController::open_close_doors_cb, this);
+			set_vel_doors_server = rosNode.advertiseService("model_dynamics_manager/doors/set_vel", &DynamicsController::set_vel_doors_cb, this);
+
+			target_floor_elev_server = rosNode.advertiseService("model_dynamics_manager/elevators/target_floor", &DynamicsController::target_floor_elev_cb, this);
+			set_elev_props_server = rosNode.advertiseService("model_dynamics_manager/elevators/set_props", &DynamicsController::set_elev_props_cb, this);
+			open_close_elev_doors_server = rosNode.advertiseService("model_dynamics_manager/elevators/open_close_elev", &DynamicsController::open_close_elev_cb, this);		
+		}
+
+		bool open_close_doors_cb(dynamic_models::OpenCloseDoors::Request &req, dynamic_models::OpenCloseDoors::Response &res)
+		{
+			int groupIndex = getGroupIndex(req.group_name);
+
+			if (groupIndex == INDEX_NOT_FOUND) {
+				ROS_ERROR("Open Door Service Failed: The specified group does not exist");
+				return false;
+			}
+
+			ControlGroup currGroup = groups.at(groupIndex);
+
+			// Publish the active doors in the group
+			std_msgs::UInt32MultiArray active_doors = uintVectorToStdMsgs(currGroup.getActiveUnits());
+			door_active_pub.publish(active_doors);
+
+			// Publish door vels:
+			geometry_msgs::Twist cmd_vel;
+
+			if (req.state == STATE_OPEN) {
+				cmd_vel.linear.x = -DEFAULT_SLIDE_SPEED;
+				cmd_vel.linear.y = -DEFAULT_SLIDE_SPEED;
+				cmd_vel.angular.z = -DEFAULT_FLIP_SPEED;
+			} else {
+				cmd_vel.linear.x = DEFAULT_SLIDE_SPEED;
+				cmd_vel.linear.y = DEFAULT_SLIDE_SPEED;
+				cmd_vel.angular.z = DEFAULT_FLIP_SPEED;
+			}
+
+			door_cmd_vel_pub.publish(cmd_vel);
+
+			return true;
+		}
+
+		bool set_vel_doors_cb(dynamic_models::SetVelDoors::Request &req, dynamic_models::SetVelDoors::Response &res)
+		{
+			return true;
+		}
+
+		bool target_floor_elev_cb(dynamic_models::TargetFloorElev::Request &req, dynamic_models::TargetFloorElev::Response &res)
+		{
+			return true;
+		}
+
+		bool set_elev_props_cb(dynamic_models::SetElevProps::Request &req, dynamic_models::SetElevProps::Response &res)
+		{
+			return true;
+		}
+
+		bool open_close_elev_cb(dynamic_models::OpenCloseElevDoors::Request &req, dynamic_models::OpenCloseElevDoors::Response &res)
+		{
+			return true;
+		}
+
+		void setupControlTopics()
+		{
+			door_cmd_vel_pub = rosNode.advertise<geometry_msgs::Twist>("/door_controller/command", 5);
+			door_active_pub = rosNode.advertise<std_msgs::UInt32MultiArray>("/door_controller/active", 5);
+
+		    elev_cmd_floor_pub = rosNode.advertise<std_msgs::Int32>("/elevator_controller/target_floor", 5);
+		    elev_active_pub = rosNode.advertise<std_msgs::UInt32MultiArray>("elevator_controller/active", 5);
+		    elev_param_pub = rosNode.advertise<std_msgs::Float32MultiArray>("elevator_controller/param", 5);
+		    elev_door_pub = rosNode.advertise<std_msgs::UInt8>("/elevator_controller/door", 5);
+		}
+
+		std_msgs::UInt32MultiArray uintVectorToStdMsgs(std::vector<uint32_t> active_units)
+		{
+			std_msgs::UInt32MultiArray active_list;
+
+			for (int i=0; i<active_units.size(); i++)
+			{	
+				active_list.data.push_back(active_units.at(i));
+			}
+
+			return active_list;
 		}
 
 		bool add_control_group_cb(dynamic_models::AddGroup::Request &req, dynamic_models::AddGroup::Response &res)
 		{	
-			GroupType type = parseGroupType(req.type);
+			GroupType type = parseGroupType(req.group.type);
 
 			if (type == INVALID) {
-				ROS_ERROR("Add Group Service Failed: Invalid group type.");
+				ROS_ERROR("Add Group Service Failed: Invalid group type");
 				return false;
 			}
 
-			ControlGroup group(req.group_name, type, req.active_units);
+			// Check if there is an exiting group with the same name, which might cause conflicts:
+			
+			int groupIndex = getGroupIndex(req.group.group_name);
+			if (groupIndex != INDEX_NOT_FOUND) {
+				ROS_ERROR("Add Group Service Failed: The specified group name already exists");
+				return false;				
+			}
+
+			ControlGroup group(req.group.group_name, type, req.group.active_units);
+			groups.push_back(group);
+
+			return true;
 		}
 
 		bool delete_control_group_cb(dynamic_models::DeleteGroup::Request &req, dynamic_models::DeleteGroup::Response &res)
 		{
+			int groupIndex = getGroupIndex(req.group_name);
 
+			if (groupIndex == INDEX_NOT_FOUND) {
+				ROS_ERROR("Delete Group Service Failed: The specified group does not exist");
+				return false;
+			}
+
+			groups.erase(groups.begin() + groupIndex);
+
+			return true;
+		}
+
+		bool list_groups_cb(dynamic_models::ListGroups::Request &req, dynamic_models::ListGroups::Response &res)
+		{
+			for (int i=0; i<groups.size(); i++) {
+				dynamic_models::ControlGroup item;
+
+				item.group_name = groups.at(i).getGroupName();
+				item.type = groups.at(i).getType();
+				item.active_units = groups.at(i).getActiveUnits();
+
+				res.groups.push_back(item);
+			}
+			
+			return true;
 		}
 
 		GroupType parseGroupType(std::string type_str)
@@ -61,6 +207,20 @@ class DynamicsController
 			} else {
 				return INVALID; // invalid type in request
 			}
+		}
+
+		int getGroupIndex(std::string group_name)
+		{
+			int groupIndex = INDEX_NOT_FOUND;
+
+			for (int i=0; i<groups.size(); i++) {
+				if (groups.at(i).getGroupName().compare(group_name) == 0) {
+					groupIndex = i;
+					return groupIndex;
+				}
+			}
+
+			return groupIndex;
 		}
 
 		void start()
